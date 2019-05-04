00329ad8ca5755e11fad0ea57119ef83f2a25ea6
Sally Beckwith <sallypbeckwith@gmail.com>
2019-05-04 15:44:03 -0400

    titles of existing notes are listed, but no links yet

--- public/js/displaySavedArticles.js
+++ public/js/displaySavedArticles.js
@@ -11,43 +11,41 @@ $(document).on("click", ".notes-btn", function() {
   // Save the id from the p tag
   // var thisId = $(this).attr("data-id");
   // console.log(thisId);
-
+  
   // Now make an ajax call for the Article
   $.ajax({
     method: "GET",
     url: "/articles/" + thisId
   })
-    // With that done, add the note information to the page
-    .then(function(data) {
-
-      console.log(data);
-      // return(data);
-
-// THIS LOGIC NEEDS TO MOVE TO THE displayNotes.handlebars file
-
-      let notesForm = `
-        <h2>${data.title}</h2>
+  // With that done, add the note form to the page
+  .then(function(data) {
+    $("#disp-notes-form").empty();
+    // If there are notes in the article, provide links to them for viewing and editing
+    if (data.notes.length > 0) {
+      let noteTitleStr = "";
+      data.notes.forEach((n) => {
+        noteTitleStr += `
+        <div class="row">
+        <div class='col-md-1'></div>
+  
+        <p class="col-md-9 p-0 m-0 note-titles">${n.title}</p>
+        <button class="col-md-1 p-0 m-0 delete-note">X</button>
+       </div>
+        `
+      })
+  
+      console.log(noteTitleStr);
+      $("#disp-notes-form").append(noteTitleStr);
+    }
+    let notesForm = `
+        <p id="note-title" class="mt-2" >${data.title}</p>
         <input id='titleinput' name='title' >
         <textarea id='bodyinput' name='body'></textarea>
-        <button data-id=${data._id} id='savenote'>Save Note</button>
+        <button data-id=${data._id} id='savenote'>Add Note</button>
       `;
 
       $("#disp-notes-form").append(notesForm);
 
-      // // If there are notes in the article, provide links to them for viewing and editing
-      // if (data.notes.length > 0) {
-      //   let noteTitleStr = "";
-      //   data.notes.forEach((n) => {
-      //     noteTitleStr += `<h4>${n.title}</h4>`
-      //   })
-
-      //   console.log(noteTitleStr);
-      //   $("#notes").prepend(noteTitleStr);
-      //   // // Place the title of the note in the title input
-      //   // $("#titleinput").val(data.note.title);
-      //   // // Place the body of the note in the body textarea
-      //   // $("#bodyinput").val(data.note.body);
-      // }
       //=================================================
     });
 });
@@ -58,9 +56,6 @@ $(document).on("click", ".notes-btn", function() {
 $(document).on("click", "#savenote", function() {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
-  console.log('saving note... ');
-  console.log(thisId);
-  console.log($("#titleinput").val());
 
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
