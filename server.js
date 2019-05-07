9d197cd3cec7293c23c71cf5ced9f45c85d49a4c
Sally Beckwith <sallypbeckwith@gmail.com>
2019-05-06 22:39:54 -0400

    attempt to get deployed mongodb code working

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
 // mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
+var MONGODB_URI = "mongodb://heroku_4bmg1nff:vpgpa70q8g15tjpg6ice9viutl@ds151876.mlab.com:51876/heroku_4bmg1nff" || "mongodb://localhost/newsScraper";
 mongoose.connect(MONGODB_URI);
 
 
